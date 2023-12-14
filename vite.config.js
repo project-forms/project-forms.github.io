3bccb4dbf6c77d225e7d1c36fc64ee3addddaab1
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2023-12-14 14:11:42 -0800

    test: initial version (#10)
    Co-authored-by: Drew Powers <1369770+drwpow@users.noreply.github.com>

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from "vite";
+// @ts-check
+
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    environment: "jsdom",
+    setupFiles: ["./vitest.setup.js"],
+    restoreMocks: true,
+  },
   plugins: [react()],
   build: {
     // for github pages deployment compatibility
---
+++ vitest.setup.js
@@ -0,0 +1,3 @@
+globalThis.CSS = {
+  supports() {},
+};
