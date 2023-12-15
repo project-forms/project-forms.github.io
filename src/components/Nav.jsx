c99240c095cb7b66846db8a4c8d95da23494f4fb
Oscar Dominguez <dominguez.celada@gmail.com>
2023-12-15 17:57:54 +0100

    fix(components): restore <Nav> missing return statement

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -13,31 +13,35 @@ import useAuthState from "../hooks/use-auth-state";
 export default function Nav() {
   const { authState, logout } = useAuthState();
 
-  <Header>
-    <Header.Item full>
-      <Header.Link href="https://github.com/project-forms">
-        <StyledOcticon icon={TableIcon} size={32} sx={{ mr: 2 }} />
-        <span>Project Forms</span>
-      </Header.Link>
-    </Header.Item>
-    {authState.user?.avatar_url && (
-      <Header.Item>
-        <ActionMenu>
-          <ActionMenu.Anchor>
-            <IconButton
-              sx={{ p: 0 }}
-              icon={() => <Avatar src={authState.user.avatar_url} size={32} />}
-              variant="invisible"
-            />
-          </ActionMenu.Anchor>
-
-          <ActionMenu.Overlay>
-            <ActionList onClick={logout}>
-              <ActionList.Item>Sign Out</ActionList.Item>
-            </ActionList>
-          </ActionMenu.Overlay>
-        </ActionMenu>
+  return (
+    <Header>
+      <Header.Item full>
+        <Header.Link href="https://github.com/project-forms">
+          <StyledOcticon icon={TableIcon} size={32} sx={{ mr: 2 }} />
+          <span>Project Forms</span>
+        </Header.Link>
       </Header.Item>
-    )}
-  </Header>;
+      {authState.user?.avatar_url && (
+        <Header.Item>
+          <ActionMenu>
+            <ActionMenu.Anchor>
+              <IconButton
+                sx={{ p: 0 }}
+                icon={() => (
+                  <Avatar src={authState.user.avatar_url} size={32} />
+                )}
+                variant="invisible"
+              />
+            </ActionMenu.Anchor>
+
+            <ActionMenu.Overlay>
+              <ActionList onClick={logout}>
+                <ActionList.Item>Sign Out</ActionList.Item>
+              </ActionList>
+            </ActionMenu.Overlay>
+          </ActionMenu>
+        </Header.Item>
+      )}
+    </Header>
+  );
 }
