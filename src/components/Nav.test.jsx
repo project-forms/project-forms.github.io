d4fcd769aa4c9f0a26bd7cab38281cd425ad38f5
Oscar Dominguez <dominguez.celada@gmail.com>
2023-12-17 10:57:31 +0100

    test(components): add test coverage for `<Nav>` component

--- src/components/Nav.test.jsx
+++ src/components/Nav.test.jsx
@@ -1,9 +1,34 @@
-import { describe, expect, test } from "vitest";
+import { describe, expect, test, beforeAll, vi } from "vitest";
 import { render } from "@testing-library/react";
 import Nav from "./Nav.jsx";
 
 describe("<Nav>", () => {
-  test("renders expected HTML", async () => {
-    expect(render(<Nav />)).toMatchSnapshot();
+  // This prevents having the error of ReferenceError: ResizeObserver is not defined
+  // https://github.com/jsdom/jsdom/issues/3368#issuecomment-1147970817
+  beforeAll(() => {
+    global.ResizeObserver = class ResizeObserver {
+      observe() {
+        // do nothing
+      }
+      unobserve() {
+        // do nothing
+      }
+      disconnect() {
+        // do nothing
+      }
+    };
+  });
+
+  test("renders expected HTML when 'avatarURL' is not provided", async () => {
+    const { asFragment } = render(<Nav />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  test("renders expected HTML when 'avatarURL' is provided", async () => {
+    const { asFragment } = render(
+      <Nav avatarUrl={"random-avatar.png"} onLogout={vi.fn()} />
+    );
+
+    expect(asFragment()).toMatchSnapshot();
   });
 });
