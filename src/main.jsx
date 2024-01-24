01087197b49a4db0f1e3d7744d6421fee7da93a7
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2024-01-24 09:52:26 -0800

    refactor: create `root.jsx` to make it testable

--- src/main.jsx
+++ src/main.jsx
@@ -1,48 +1,6 @@
-// @ts-check
-
 import ReactDOM from "react-dom/client";
 
-import { BaseStyles, ThemeProvider } from "@primer/react";
+import Root from "./root.jsx";
 
-import Root from "./routes/root.jsx";
-import "./index.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Login from "./routes/login.jsx";
-import NewIssueForm from "./routes/new.jsx";
-import App from "./App.jsx";
-import { ErrorBoundary } from "react-error-boundary";
-import ErrorFallback from "./components/ErrorFallback.jsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        index: true,
-        element: <Root />,
-      },
-      {
-        path: "/login",
-        element: <Login />,
-      },
-      {
-        path: "/project-forms/:repo/projects/:project_number/issues/new",
-        element: (
-          <ErrorBoundary FallbackComponent={ErrorFallback}>
-            <NewIssueForm />
-          </ErrorBoundary>
-        ),
-        errorElement: <p>Error</p>,
-      },
-    ],
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <ThemeProvider>
-    <BaseStyles>
-      <RouterProvider router={router} />
-    </BaseStyles>
-  </ThemeProvider>
-);
+const rootElement = document.getElementById("root");
+ReactDOM.createRoot(rootElement).render(<Root />);
