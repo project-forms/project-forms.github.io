1695b05b42f954e980facfc5ad6846cb1a45a1d5
Oscar Dominguez <dominguez.celada@gmail.com>
2024-02-11 14:17:08 +0100

    test(integration): add test coverage for 'Access Error' in `/new` route

--- src/test/intergration.test.jsx
+++ src/test/intergration.test.jsx
@@ -1,3 +1,4 @@
+// @ts-check
 import {
   describe,
   expect,
@@ -8,10 +9,8 @@ import {
   vi,
 } from "vitest";
 import { render, waitFor } from "@testing-library/react";
-import { BaseStyles, ThemeProvider } from "@primer/react";
 import { setupServer } from "msw/node";
 import { http, HttpResponse, graphql } from "msw";
-import { OctokitProvider } from "../components/octokit-provider.js";
 import Root from "../root.jsx";
 import UserResponse from "./fixtures/api.github.com/user.json";
 
@@ -55,104 +54,177 @@ describe("App", () => {
     server.close();
   });
 
-  test("/project-forms/demo/projects/1/issues/new?code=123", async () => {
-    const mockLocation = {
-      pathname: "/project-forms/demo/projects/1/issues/new",
-      origin: "http://localhost.test",
-      search: "?code=123",
-      searchParams: {
-        get: (key) => {
-          if (key === "code") {
-            return "123";
+  describe("/project-forms/demo/projects/1/issues/new?code=123", async () => {
+    test("renders the form successfully", async () => {
+      server.use(
+        http.get("https://api.github.com/user", () => {
+          return HttpResponse.json(UserResponse);
+        }),
+
+        http.post(
+          "http://localhost.test/api/github/oauth/token",
+          async ({ request }) => {
+            const requestBody = await request.json();
+            expect(requestBody).toStrictEqual({
+              code: "123",
+            });
+
+            return HttpResponse.json({
+              authentication: {
+                token: "secret_123",
+              },
+            });
           }
-        },
-      },
-      href: "http://localhost.test/project-forms/demo/projects/1/issues/new?code=123",
-    };
-
-    server.use(
-      http.get("https://api.github.com/user", () => {
-        return HttpResponse.json(UserResponse);
-      }),
-
-      http.post(
-        "http://localhost.test/api/github/oauth/token",
-        async ({ request }) => {
-          const requestBody = await request.json();
-          expect(requestBody).toStrictEqual({
-            code: "123",
+        ),
+        githubGraphQL.query("verifyAccess", ({ variables }) => {
+          expect(variables).toStrictEqual({
+            owner: "project-forms",
+            repo: "demo",
+            projectNumber: 1,
           });
 
           return HttpResponse.json({
-            authentication: {
-              token: "secret_123",
-            },
-          });
-        }
-      ),
-      githubGraphQL.query("verifyAccess", ({ variables }) => {
-        expect(variables).toStrictEqual({
-          owner: "project-forms",
-          repo: "demo",
-          projectNumber: 1,
-        });
-
-        return HttpResponse.json({
-          data: {
-            repository: "randomRepo",
-            owner: {
-              project: {
-                viewerCanUpdate: true,
-              },
-            },
-          },
-        });
-      }),
-      githubGraphQL.query("getProjectWithItems", ({ variables }) => {
-        expect(variables).toStrictEqual({
-          owner: "project-forms",
-          number: 1,
-        });
-
-        return HttpResponse.json({
-          data: {
-            userOrOrganization: {
-              projectV2: {
-                title: "My Project",
-                url: "Project URL",
-                fields: {
-                  nodes: [
-                    {
-                      name: "Field 1",
-                      dataType: "text",
-                      id: "1",
-                    },
-                    {
-                      name: "Field 2",
-                      dataType: "text",
-                      id: "2",
-                    },
-                  ],
+            data: {
+              repository: "randomRepo",
+              owner: {
+                project: {
+                  viewerCanUpdate: true,
                 },
               },
             },
-          },
-        });
-      })
-    );
+          });
+        }),
+        githubGraphQL.query("getProjectWithItems", ({ variables }) => {
+          expect(variables).toStrictEqual({
+            owner: "project-forms",
+            number: 1,
+          });
 
-    const { asFragment, getByText } = render(<Root />);
+          return HttpResponse.json({
+            data: {
+              userOrOrganization: {
+                projectV2: {
+                  title: "My Project",
+                  url: "Project URL",
+                  fields: {
+                    nodes: [
+                      {
+                        name: "Field 1",
+                        dataType: "text",
+                        id: "1",
+                      },
+                      {
+                        name: "Field 2",
+                        dataType: "text",
+                        id: "2",
+                      },
+                    ],
+                  },
+                },
+              },
+            },
+          });
+        })
+      );
 
-    expect(asFragment()).toMatchSnapshot("loading");
+      const { asFragment, getByText } = render(<Root />);
 
-    await waitFor(
-      () => {
-        const title = getByText(/My Project/);
-        expect(title).not.toBeNull();
-      },
-      { timeout: 1000 }
-    );
+      expect(asFragment()).toMatchSnapshot("loading");
 
-    expect(asFragment()).toMatchSnapshot("loaded");
+      await waitFor(
+        () => {
+          const title = getByText(/My Project/);
+          expect(title).not.toBeNull();
+        },
+        { timeout: 1000 }
+      );
+
+      expect(asFragment()).toMatchSnapshot("loaded");
+    });
+
+    test("renders error if the authenticated user has no access", async () => {
+      server.use(
+        http.get("https://api.github.com/user", () => {
+          return HttpResponse.json(UserResponse);
+        }),
+
+        http.post(
+          "http://localhost.test/api/github/oauth/token",
+          async ({ request }) => {
+            const requestBody = await request.json();
+            expect(requestBody).toStrictEqual({
+              code: "123",
+            });
+
+            return HttpResponse.json({
+              authentication: {
+                token: "secret_123",
+              },
+            });
+          }
+        ),
+        githubGraphQL.query("verifyAccess", ({ variables }) => {
+          expect(variables).toStrictEqual({
+            owner: "project-forms",
+            repo: "demo",
+            projectNumber: 1,
+          });
+
+          return HttpResponse.json({
+            data: {
+              repository: "randomRepo",
+              owner: {
+                project: {
+                  viewerCanUpdate: false,
+                },
+              },
+            },
+          });
+        }),
+        githubGraphQL.query("getProjectWithItems", ({ variables }) => {
+          expect(variables).toStrictEqual({
+            owner: "project-forms",
+            number: 1,
+          });
+
+          return HttpResponse.json({
+            data: {
+              userOrOrganization: {
+                projectV2: {
+                  title: "My Project",
+                  url: "Project URL",
+                  fields: {
+                    nodes: [
+                      {
+                        name: "Field 1",
+                        dataType: "text",
+                        id: "1",
+                      },
+                      {
+                        name: "Field 2",
+                        dataType: "text",
+                        id: "2",
+                      },
+                    ],
+                  },
+                },
+              },
+            },
+          });
+        })
+      );
+
+      const { asFragment, getByText } = render(<Root />);
+
+      await waitFor(
+        () => {
+          const title = getByText(/Access Error/);
+          expect(title).not.toBeNull();
+        },
+        { timeout: 1000 }
+      );
+
+      expect(asFragment()).toMatchSnapshot();
+    });
   });
 });
