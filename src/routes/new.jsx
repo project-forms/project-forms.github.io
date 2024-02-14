9cf572daf50a6a9c3df269b26dbcd058087e4665
Oscar Dominguez <dominguez.celada@gmail.com>
2024-02-14 17:53:25 +0100

    fix(routes): `/new` loader returns promises to render a loading fallback with `React.Suspense`
    'hasAccess' error will be managed by react-router error in a follow-up PR

--- src/routes/new.jsx
+++ src/routes/new.jsx
@@ -1,5 +1,5 @@
 // @ts-check
-import { useState } from "react";
+import { useState, Suspense } from "react";
 import {
   ActionList,
   Box,
@@ -7,6 +7,7 @@ import {
   Heading,
   Link,
   StyledOcticon,
+  Spinner,
 } from "@primer/react";
 import Project from "github-project";
 import Nav from "../components/Nav.jsx";
@@ -19,6 +20,8 @@ import {
   useParams,
   useLoaderData,
   redirect,
+  defer,
+  Await,
 } from "react-router-dom";
 import { Octokit } from "@octokit-next/core";
 
@@ -144,53 +147,16 @@ export const loader = async ({ params }) => {
     }
 
     try {
-      const data = await octokit.graphql(GET_PROJECTS_WITH_ITEMS_QUERY, {
+      const projectData = octokit.graphql(GET_PROJECTS_WITH_ITEMS_QUERY, {
         owner: parameters.owner,
         number: parameters.projectNumber,
       });
-      // @ts-expect-error
-      const project = data.userOrOrganization.projectV2;
 
-      console.log(data.userOrOrganization);
-
-      const projectData = {
-        title: project.title,
-        url: project.url,
-        fields: project.fields.nodes.reduce((fields, field) => {
-          if (!SUPPORTED_PROJECT_FIELD_TYPES.includes(field.dataType)) {
-            return fields;
-          }
-
-          function iterationToOption(iteration) {
-            return {
-              id: iteration.id,
-              name: iteration.id,
-              humanName: `${iteration.title} (${iteration.duration} days from ${iteration.startDate}))`,
-            };
-          }
-
-          return [
-            ...fields,
-            {
-              id: field.id,
-              name: field.name,
-              type: field.dataType,
-              options: field.options
-                ? field.options
-                : field.configuration
-                ? [
-                    ...field.configuration.iterations.map(iterationToOption),
-                    ...field.configuration.completedIterations.map(
-                      iterationToOption
-                    ),
-                  ]
-                : undefined,
-            },
-          ];
-        }, []),
-      };
-
-      return { ...projectData, token, user: await createStore("user").get() };
+      return defer({
+        projectData,
+        token,
+        user: createStore("user").get(),
+      });
     } catch (error) {
       const message = error.errors ? error.errors[0].message : error.message;
 
@@ -214,185 +180,153 @@ export default function NewIssuePage() {
   const [submittedIssueUrl, setSubmittedIssueUrl] = useState("");
   const [accessError, setAccessError] = useState(null);
   const navigate = useNavigate();
-  const { title, url, fields, token, user } = useLoaderData();
-
-  /**
-   * @type {import("..").Store<import("..").StoreData>}
-   */
-
-  if (accessError) {
-    const { owner, repo, projectNumber } = parameters;
-
-    return (
-      <Box m="4" justifyContent="center">
-        <Heading sx={{ mb: 4 }}>Access Error</Heading>
-
-        <ActionList>
-          <ActionList.Item>
-            <ActionList.LeadingVisual>
-              {accessError.hasRepoAccess ? (
-                <StyledOcticon
-                  icon={CheckCircleFillIcon}
-                  size={32}
-                  color="success"
-                />
-              ) : (
-                <StyledOcticon
-                  icon={XCircleFillIcon}
-                  size={32}
-                  color="danger"
-                />
-              )}
-            </ActionList.LeadingVisual>
-            access to{" "}
-            <Link
-              href={`https://github.com/${owner}/${repo}`}
-            >{`${owner}/${repo}`}</Link>
-          </ActionList.Item>
-          <ActionList.Item>
-            <ActionList.LeadingVisual>
-              {accessError.hasProjectReadAccess ? (
-                <StyledOcticon
-                  icon={CheckCircleFillIcon}
-                  size={32}
-                  color="success"
-                />
-              ) : (
-                <StyledOcticon
-                  icon={XCircleFillIcon}
-                  size={32}
-                  color="danger"
-                />
-              )}
-            </ActionList.LeadingVisual>
-            Read access to project{" "}
-            {url && (
-              <Link href={url}>
-                #{projectNumber} {title}
-              </Link>
-            )}
-          </ActionList.Item>
-          <ActionList.Item>
-            <ActionList.LeadingVisual>
-              {accessError.hasProjectWriteAccess ? (
-                <StyledOcticon
-                  icon={CheckCircleFillIcon}
-                  size={32}
-                  color="success"
-                />
-              ) : (
-                <StyledOcticon
-                  icon={XCircleFillIcon}
-                  size={32}
-                  color="danger"
-                />
-              )}
-            </ActionList.LeadingVisual>
-            Write access to project{" "}
-            {url && (
-              <Link href={url}>
-                #{projectNumber} {title}
-              </Link>
-            )}
-          </ActionList.Item>
-        </ActionList>
-      </Box>
-    );
-  }
-
-  /**
-   * @param {Record<string, unknown>} data
-   */
-  async function onSubmit(data) {
-    // TODO: handle errors
-
-    setIsSubmittingIssue(true);
-    const { title, body, ...projectFields } = data;
-
-    console.log("Creating issue ...");
-    const octokit = new Octokit({ auth: token });
-
-    // Create the issue
-    const { data: issue } = await octokit.request(
-      "POST /repos/{owner}/{repo}/issues",
-      {
-        owner: owner,
-        repo: repo,
-        title: String(title),
-        body: String(body),
-      }
-    );
-    console.log("Issue created: %s", issue.html_url);
-
-    console.log("Adding issue to project ...");
-
-    const fields = Object.fromEntries(
-      Object.keys(projectFields).map((name) => [name, name])
-    );
-    console.log({ fields, projectFields });
-
-    // Add the issue to the project
-    const project = new Project({
-      octokit,
-      owner: owner,
-      number: parameters.projectNumber,
-      fields,
-    });
-
-    try {
-      // @ts-expect-error
-      await project.items.add(issue.node_id, projectFields);
-    } catch (error) {
-      console.error(error.details);
-      throw error;
-    }
-
-    console.log("Issue added to project: %s", url);
-
-    setSubmittedIssueUrl(issue.html_url);
-  }
+  const { projectData, user, token } = useLoaderData();
 
   return (
-    <>
-      <Nav
-        avatarUrl={user?.avatar_url}
-        onLogout={() => {
-          navigate("/login");
-        }}
-      />
-      <Box
-        sx={{
-          bg: "canvas.inset",
-          borderBottom: "1px solid",
-          borderBottomColor: "border.default",
-        }}
+    <Suspense
+      fallback={
+        <Box mt="4" display="flex" justifyContent="center">
+          <Spinner />
+        </Box>
+      }
+    >
+      <Await
+        resolve={Promise.all([projectData, user])}
+        errorElement={<p>Error loading package location!</p>}
       >
-        <ContentWrapper>
-          <Breadcrumbs>
-            <Breadcrumbs.Item href={`https://github.com/${owner}`}>
-              {owner}
-            </Breadcrumbs.Item>
-            <Breadcrumbs.Item
-              href={`https://github.com/project-forms/${repo}`}
-              selected
-            >
-              {repo}
-            </Breadcrumbs.Item>
-          </Breadcrumbs>
-          <Heading sx={{ fontSize: 2 }}>
-            Submit to project{" "}
-            <Link href={url}>
-              #{parameters.projectNumber} {title}
-            </Link>
-          </Heading>
-        </ContentWrapper>
-      </Box>
-      <NewIssueForm
-        onSubmit={onSubmit}
-        submittedIssueUrl={submittedIssueUrl}
-        projectFields={fields}
-        isSubmittingIssue={isSubmittingIssue}
-      />
-    </>
+        {([projectData, user]) => {
+          const project = projectData.userOrOrganization.projectV2;
+          const { title, url } = project;
+          const fields = project.fields.nodes.reduce((fields, field) => {
+            if (!SUPPORTED_PROJECT_FIELD_TYPES.includes(field.dataType)) {
+              return fields;
+            }
+
+            function iterationToOption(iteration) {
+              return {
+                id: iteration.id,
+                name: iteration.id,
+                humanName: `${iteration.title} (${iteration.duration} days from ${iteration.startDate}))`,
+              };
+            }
+
+            return [
+              ...fields,
+              {
+                id: field.id,
+                name: field.name,
+                type: field.dataType,
+                options: field.options
+                  ? field.options
+                  : field.configuration
+                  ? [
+                      ...field.configuration.iterations.map(iterationToOption),
+                      ...field.configuration.completedIterations.map(
+                        iterationToOption
+                      ),
+                    ]
+                  : undefined,
+              },
+            ];
+          }, []);
+
+          /**
+           * @param {Record<string, unknown>} data
+           */
+          async function onSubmit(data) {
+            // TODO: handle errors
+
+            setIsSubmittingIssue(true);
+            const { title, body, ...projectFields } = data;
+
+            console.log("Creating issue ...");
+            const octokit = new Octokit({ auth: token });
+
+            // Create the issue
+            const { data: issue } = await octokit.request(
+              "POST /repos/{owner}/{repo}/issues",
+              {
+                owner: owner,
+                repo: repo,
+                title: String(title),
+                body: String(body),
+              }
+            );
+            console.log("Issue created: %s", issue.html_url);
+
+            console.log("Adding issue to project ...");
+
+            const fields = Object.fromEntries(
+              Object.keys(projectFields).map((name) => [name, name])
+            );
+            console.log({ fields, projectFields });
+
+            // Add the issue to the project
+            const project = new Project({
+              octokit,
+              owner: owner,
+              number: parameters.projectNumber,
+              fields,
+            });
+
+            try {
+              // @ts-expect-error
+              await project.items.add(issue.node_id, projectFields);
+            } catch (error) {
+              console.error(error.details);
+              throw error;
+            }
+
+            console.log("Issue added to project: %s", url);
+
+            setSubmittedIssueUrl(issue.html_url);
+          }
+
+          return (
+            <>
+              <Nav
+                avatarUrl={user?.avatar_url}
+                onLogout={() => {
+                  navigate("/login");
+                }}
+              />
+              <Box
+                sx={{
+                  bg: "canvas.inset",
+                  borderBottom: "1px solid",
+                  borderBottomColor: "border.default",
+                }}
+              >
+                <ContentWrapper>
+                  <Breadcrumbs>
+                    <Breadcrumbs.Item href={`https://github.com/${owner}`}>
+                      {owner}
+                    </Breadcrumbs.Item>
+                    <Breadcrumbs.Item
+                      href={`https://github.com/project-forms/${repo}`}
+                      selected
+                    >
+                      {repo}
+                    </Breadcrumbs.Item>
+                  </Breadcrumbs>
+                  <Heading sx={{ fontSize: 2 }}>
+                    Submit to project{" "}
+                    <Link href={url}>
+                      #{parameters.projectNumber} {title}
+                    </Link>
+                  </Heading>
+                </ContentWrapper>
+              </Box>
+              <NewIssueForm
+                onSubmit={onSubmit}
+                submittedIssueUrl={submittedIssueUrl}
+                projectFields={fields}
+                isSubmittingIssue={isSubmittingIssue}
+              />
+            </>
+          );
+        }}
+      </Await>
+    </Suspense>
   );
 }
